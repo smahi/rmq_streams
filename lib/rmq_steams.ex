1e4659c90e471cb3f640dc8f5bdc136464dc8bda
Abesse Smahi <contact@smahi.net>
2024-04-18 13:08:10 +0100

    Initial commit

---
+++ lib/rmq_steams.ex
@@ -0,0 +1,18 @@
+defmodule RmqSteams do
+  @moduledoc """
+  Documentation for `RmqSteams`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RmqSteams.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rmq_steams_test.exs
@@ -0,0 +1,8 @@
+defmodule RmqSteamsTest do
+  use ExUnit.Case
+  doctest RmqSteams
+
+  test "greets the world" do
+    assert RmqSteams.hello() == :world
+  end
+end
